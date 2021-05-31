704351ec037849fd3b09bac15269179195dddc7b
Akshada2205 <84906640+Akshada2205@users.noreply.github.com>
2021-05-30 21:42:38 -0700

    Update Akshada.java

--- Akshu/src/Akshada.java
+++ Akshu/src/Akshada.java
@@ -4,6 +4,7 @@ public class Akshada {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("UMM u r so beautiful....");
+		System.out.println("I want non veg food");
 	}
 
 }
